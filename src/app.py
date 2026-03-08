14de47aa88d9511ff3fbe0282f10ffcf6c9b72bf
Alessandra Mantellini <mantellinialessandra13@gmail.com>
2026-03-08 17:54:34 +0000

    exploring data

--- src/app.py
+++ src/app.py
@@ -2,3 +2,10 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+# IMPORTO LA LIBRERÍA PANDAS
+import pandas as pd
+
+# CARGO LOS DATOS
+total_data = pd.read_csv('AB_NYC_2019.csv')
+total_data
+
